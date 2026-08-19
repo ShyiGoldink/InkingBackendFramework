8a7256f61a1f792105a8863e8bf1c9ebaf25239e
ShyiGoldink <2430687365@qq.com>
2026-08-19 20:56:00 +0800

    fix:强制修改控制台的输出格式为UTF-8

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,17 @@
 #include "core/Application.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 int main()
 {
+#ifdef _WIN32
+    // 与 README 保持一致：源码与程序输出统一使用 UTF-8。
+    // 不设置的话，Windows 控制台默认按 GBK 解码，中文会显示成乱码。
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+#endif
     Application app;
     app.run();
     return 0;
